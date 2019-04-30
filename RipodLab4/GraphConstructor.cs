e7d7f8340113d690d2525df0f4e101ab115981ac
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-30 22:01:46 +0300

    Add graph constructor class

---
+++ RipodLab4/GraphConstructor.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RipodLab4
+{
+    public class GraphConstructor
+    {
+        public Graph ConstructGraph(DataGridView nodes, string types, DataGridView times)
+        {
+            var graph = new Graph();
+            var nodesArr = ReadNodes(types, times);
+            FormGraph(nodesArr, nodes);
+            graph.Roots = GetRoots(nodesArr);
+
+            return graph;
+        }
+
+        private IList<Node> GetRoots(Node[] nodesArr)
+        {
+            var roots = new List<Node>();
+
+            for (var i = 0; i < nodesArr.Length; i++)
+            {
+                if (nodesArr[i].Parents.Count == 0)
+                {
+                    roots.Add(nodesArr[i]);
+                }
+            }
+
+            return roots;
+        }
+
+        private void FormGraph(Node[] nodesArr, DataGridView nodes)
+        {
+            for (var i = 0; i < nodesArr.Length; i++)
+            {
+                var index = FildColumnIndex(nodes, i);
+                if (index > -1)
+                {
+                    nodesArr[i].Next = nodesArr[index];
+                    nodesArr[index].Parents.Add(nodesArr[i]);
+                }
+                else
+                {
+                    nodesArr[i].Next = null;
+                }
+            }
+        }
+
+        private Node[] ReadNodes(string types, DataGridView times)
+        {
+            var nodesArr = new Node[types.Length];
+
+            for (var i = 0; i < nodesArr.Length; i++)
+            {
+                nodesArr[i] = new Node
+                {
+                    Value = (i + 1).ToString(),
+                    Type = types[i].ToString(),
+                    Time = double.Parse(times[1, i].Value.ToString())
+                };
+            }
+
+            return nodesArr;
+        }
+
+        private int FildColumnIndex(DataGridView nodes, int rowIndex)
+        {
+            for (var i = 0; i < nodes.ColumnCount; i++)
+            {
+                if (nodes[i, rowIndex].Value.Equals("1"))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
